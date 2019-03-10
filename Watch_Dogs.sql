ee964d8b1bf88f8f727d2673de3d2aeff295cb11
HouJie <h.j.13.new@gmail.com>
2019-03-10 18:59:38 +0800

    update : update db.sql

--- Watch_Dogs.sql
+++ Watch_Dogs.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80011
  File Encoding         : 65001
 
- Date: 26/02/2019 19:49:16
+ Date: 10/03/2019 18:53:55
 */
 
 SET NAMES utf8mb4;
@@ -22,45 +22,47 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `Host_info`;
 CREATE TABLE `Host_info`  (
-  `host_id` int(10) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT COMMENT '主机id',
+  `host_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主机id',
   `host` char(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '主机ip',
   `user` char(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '用户名',
-  `password` char(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '密码',
-  `port` tinyint(255) NOT NULL DEFAULT 0 COMMENT '端口',
+  `password` char(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '密码',
+  `port` int(255) NOT NULL DEFAULT 0 COMMENT '端口',
   `system` char(65) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '系统',
   `kernel` char(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '内核',
   `CPU_info` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT 'CPU信息',
-  `mem_KB` int(255) NOT NULL DEFAULT 0 COMMENT '内存大小',
+  `mem_M` float(255, 2) NOT NULL DEFAULT 0.00 COMMENT '内存大小',
   `disk_stat` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '磁盘状态',
   `default_net_device` char(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '默认网卡',
   `intranet_ip` char(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '内网ip',
   `extranet_ip` char(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '外网ip',
-  `update_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
+  `update_time` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
   PRIMARY KEY (`host_id`) USING BTREE,
   UNIQUE INDEX `UNI`(`host`, `user`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for Host_record
 -- ----------------------------
 DROP TABLE IF EXISTS `Host_record`;
 CREATE TABLE `Host_record`  (
-  `record_id` int(10) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT COMMENT '记录id',
-  `host_id` int(11) NOT NULL COMMENT '主机id',
+  `record_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '记录id',
+  `host` char(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '主机',
   `CPU` float(255, 0) NOT NULL COMMENT 'CPU百分比',
-  `CPUs` json NOT NULL COMMENT 'CPU单核数据',
-  `mem` float(255, 0) NOT NULL COMMENT '内存百分比',
-  `read_MBs` float(255, 0) NOT NULL COMMENT '读取速度',
-  `write_MBs` float(255, 0) NOT NULL COMMENT '写入速度',
-  `net_upload_kbps` float(255, 0) NOT NULL COMMENT '上传速度',
-  `net_download_kbps` float(255, 0) NOT NULL COMMENT '下载速度',
-  `lavg_1` float(255, 0) NOT NULL DEFAULT 0 COMMENT '系统1分钟负载率',
-  `lavg_5` float(255, 0) NOT NULL DEFAULT 0 COMMENT '系统5分钟负载率',
-  `lavg_15` float(255, 0) NOT NULL DEFAULT 0 COMMENT '系统15分钟负载率',
+  `CPUs` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'CPU单核数据',
+  `mem` float(255, 2) NOT NULL COMMENT '内存百分比',
+  `read_MBs` float(255, 2) NOT NULL COMMENT '读取速度',
+  `write_MBs` float(255, 2) NOT NULL COMMENT '写入速度',
+  `net_upload_kbps` float(255, 2) NOT NULL COMMENT '上传速度',
+  `net_download_kbps` float(255, 2) NOT NULL COMMENT '下载速度',
+  `lavg_1` float(255, 2) NOT NULL DEFAULT 0.00 COMMENT '系统1分钟负载率',
+  `lavg_5` float(255, 2) NOT NULL DEFAULT 0.00 COMMENT '系统5分钟负载率',
+  `lavg_15` float(255, 2) NOT NULL DEFAULT 0.00 COMMENT '系统15分钟负载率',
   `nr` char(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '系统负载比例',
+  `free_rate` float(255, 4) NOT NULL DEFAULT 0.0000 COMMENT '系统空闲率',
+  `uptime` char(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '系统启动时间',
   `record_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '记录时间',
   PRIMARY KEY (`record_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for Process
@@ -107,7 +109,7 @@ CREATE TABLE `Process_record`  (
   `net_download_kbps_min` float(255, 0) NOT NULL DEFAULT 0 COMMENT '下载速度最小值',
   `record_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '记录时间',
   PRIMARY KEY (`record_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for Process_record_cache
@@ -124,7 +126,7 @@ CREATE TABLE `Process_record_cache`  (
   `net_download_kbps` float(255, 0) NOT NULL DEFAULT 0 COMMENT '下载速度',
   `record_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '记录时间',
   PRIMARY KEY (`record_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for User
@@ -138,7 +140,7 @@ CREATE TABLE `User`  (
   `registration_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '注册时间',
   PRIMARY KEY (`user_id`) USING BTREE,
   UNIQUE INDEX `UNI_user`(`user`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for User_Host
@@ -151,7 +153,7 @@ CREATE TABLE `User_Host`  (
   `relation_time` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP COMMENT '关系建立时间',
   PRIMARY KEY (`relation_id`) USING BTREE,
   UNIQUE INDEX `UNI`(`user_id`, `host_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for User_Process
@@ -167,6 +169,6 @@ CREATE TABLE `User_Process`  (
   `relation_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '关系建立时间',
   PRIMARY KEY (`relation_id`) USING BTREE,
   UNIQUE INDEX `UNI`(`user_id`, `host_id`, `process_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
